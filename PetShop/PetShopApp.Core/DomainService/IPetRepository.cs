3efe686bf1480cb2007fe35e8a5c1f2380647486
bencematyasi <31878635+bencematyasi@users.noreply.github.com>
2018-08-29 14:17:12 +0200

    Structure first part and some intial method

---
+++ PetShop/PetShopApp.Core/DomainService/IPetRepository.cs
@@ -0,0 +1,20 @@
+﻿using PetShopApp.Core.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetShopApp.Core.DomainService
+{
+   public interface IPetRepository
+    {
+        Pet Creat(Pet pet);
+
+        Pet ReadById(int id);
+
+        List<Pet> ReadAll();
+
+        Pet Update(Pet petUpdate);
+
+        Pet Delete(int id);
+    }
+}
---
+++ PetShop/PetShopApp.Infrastructure.Static.Data/Repositories/PetRepository.cs
@@ -0,0 +1,39 @@
+﻿using PetShopApp.Core.DomainService;
+using PetShopApp.Core.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetShopApp.Infrastructure.Static.Data.Reporsitories
+{
+    public class PetRepository : IPetRepository
+    {
+        static int id = 1;
+        private List<Pet> pets = new List<Pet>();
+
+        public Pet Creat(Pet pet)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Pet Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Pet> ReadAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Pet ReadById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Pet Update(Pet petUpdate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
