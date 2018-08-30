21138e72dfcc69fddfd7cef350f2b6bec3a2adf3
bencematyasi <31878635+bencematyasi@users.noreply.github.com>
2018-08-30 13:17:03 +0200

    implement methods from IPetRepository and complete it

--- PetShop/PetShop/Program.cs
+++ PetShop/PetShop/Program.cs
@@ -1,12 +1,34 @@
-﻿using System;
+﻿using PetShopApp.Core.DomainService;
+using PetShopApp.Core.Entity;
+using PetShopApp.Infrastructure.Static.Data.Reporsitories;
+using System;
 
 namespace PetShop
 {
     class Program
     {
+        static IPetRepository petRepository;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            petRepository = new PetRepository();
+            Pet brunoDoggy = new Pet
+            {
+                Id = 1,
+                Name = "Bruno",
+                Type = "dog",
+                BirthDay = new DateTime(2010, 10, 05).Date,
+                
+                SoldDate = new DateTime(2010, 11, 05).Date,
+                Color = "blueish",
+                PreviousOwner = "Danny Boy",
+                Price = 40
+            };
+
+            Console.WriteLine("name: {0}\ntype: {1}\nBirtday: {2}\nsold date: {3}\ncolor: {4}\nPrevious owner: {5}\nprice: ${6}", brunoDoggy.Name, brunoDoggy.Type, brunoDoggy.BirthDay, brunoDoggy.SoldDate, brunoDoggy.Color, brunoDoggy.PreviousOwner, brunoDoggy.Price);
+            
+            Console.ReadLine();
+
         }
     }
 }
--- PetShop/PetShopApp.Infrastructure.Static.Data/Repositories/PetRepository.cs
+++ PetShop/PetShopApp.Infrastructure.Static.Data/Repositories/PetRepository.cs
@@ -9,31 +9,60 @@ namespace PetShopApp.Infrastructure.Static.Data.Reporsitories
     public class PetRepository : IPetRepository
     {
         static int id = 1;
-        private List<Pet> pets = new List<Pet>();
+        private List<Pet> _pets = new List<Pet>();
 
         public Pet Creat(Pet pet)
         {
-            throw new NotImplementedException();
-        }
-
-        public Pet Delete(int id)
-        {
-            throw new NotImplementedException();
+            pet.Id = id++;
+            _pets.Add(pet);
+            return pet;
         }
 
         public List<Pet> ReadAll()
         {
-            throw new NotImplementedException();
+            return _pets;  
         }
 
         public Pet ReadById(int id)
         {
-            throw new NotImplementedException();
+            foreach (var pet in _pets)
+            {
+                if (pet.Id == id)
+                {
+                    return pet;
+                }
+
+            }
+            return null;
         }
 
         public Pet Update(Pet petUpdate)
         {
-            throw new NotImplementedException();
+            var petFromDB = this.ReadById(petUpdate.Id);
+            if (petFromDB != null)
+            {
+                petFromDB.Name = petUpdate.Name;
+                petFromDB.Type = petUpdate.Type;
+                petFromDB.BirthDay = petUpdate.BirthDay;
+                petFromDB.SoldDate = petUpdate.SoldDate;
+                petFromDB.Color = petUpdate.Color;
+                petFromDB.PreviousOwner = petUpdate.PreviousOwner;
+                petFromDB.Price = petUpdate.Price;
+
+                return petFromDB;
+            }
+            return null;
+        }
+        public Pet Delete(int id)
+        {
+            var petFound = this.ReadById(id);
+            if(petFound != null)
+            {
+                _pets.Remove(petFound);
+                return petFound;
+            }
+            return null;
+
         }
     }
 }
